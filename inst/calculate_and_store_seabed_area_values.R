3c6ab1d46d50982fa167c20baade1dc37f57cf47
lucymerobinson <lucy.robinson@ccamlr.org>
2018-01-25 12:38:55 +1100

    added inst folder

---
+++ inst/calculate_and_store_seabed_area_values.R
@@ -0,0 +1,17 @@
+library(CCAMLRGIS)
+library(BERT)
+library(rgdal)
+
+# load bathymetry data
+load(paste(getwd(),"/","data/bathy_data.rda",sep=""))
+
+RS_blks <- load_RBs("GEOJSON")
+Ref_areas <- load_RefAreas("RDATA")
+Ref_areas <- Ref_areas[Ref_areas$name%in%c("HIMI","RSR")]
+
+# calculate seabed area for Research blocks 
+RB_seabed_area <- seabed_area(bathy_data,RS_blks,"GAR_Short_Label",TRUE)
+# store areas when this is done as processing is quite time consuming 
+# calculate seabed area for reference area
+Ref_area_sebed_area <- seabed_area(bathy_data,Ref_areas,"name",TRUE)
+# store areas when this is done as processing is quite time consuming
