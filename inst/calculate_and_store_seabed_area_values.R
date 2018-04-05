40bad3e113e14ceff26f41e8082d8d7b37dea731
lucymerobinson <lucy.robinson@ccamlr.org>
2018-04-05 16:39:43 +1000

    updated script used in generating seabed area estimates

--- inst/calculate_and_store_seabed_area_values.R
+++ inst/calculate_and_store_seabed_area_values.R
@@ -2,16 +2,22 @@ library(CCAMLRGIS)
 library(BERT)
 library(rgdal)
 
-# load bathymetry data
-load(paste(getwd(),"/","data/bathy_data.rda",sep=""))
+# load bathymetry data - request GEBCO_2014_600_1800m.zip from CCAMLR Secretariat 
+# This should include bathymetry data file GEBCO_2014_geotiff_projected_cubic_resample_500_500m_depths_600_1800m2016-04-27.tif  
+# metadata file GEBCO_2014_600_1800m_metadata.txt
 
 RS_blks <- CCAMLRGIS::load_RBs("GEOJSON")
 Ref_areas <- CCAMLRGIS::load_RefAreas("RDATA")
 Ref_areas <- CCAMLRGIS::Ref_areas[Ref_areas$name%in%c("HIMI","RSR")]
 
 # calculate seabed area for Research blocks 
-RB_seabed_area <- seabed_area(bathy_data,RS_blks,"GAR_Short_Label",TRUE)
+RB_seabed_area <- CCAMLRGIS::seabed_area(bathy_data,RS_blks,fishable_area=TRUE)
+save(RB_seabed_area,file="data/RB_seabed_area.rda")
+
 # store areas when this is done as processing is quite time consuming 
 # calculate seabed area for reference area
-Ref_area_sebed_area <- seabed_area(bathy_data,Ref_areas,"name",TRUE)
+Ref_area_sebed_area <- CCAMLRGIS::seabed_area(bathy_data,Ref_areas,fishable_area=TRUE)
 # store areas when this is done as processing is quite time consuming
+save(Ref_area_seabed_area,file="data/Ref_area_sebed_area.rda")
+
+
